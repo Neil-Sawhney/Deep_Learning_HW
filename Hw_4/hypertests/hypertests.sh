b4cdcf62dba3d2ed7a935c36f00371e263730679
Neil Sawhney <Neil@neil-sawhney.com>
2023-09-28 18:04:28 -0400

    more tests

--- Hw_4/hypertests/hypertests.sh
+++ Hw_4/hypertests/hypertests.sh
@@ -1,5 +1,3 @@
-./main.py ./runners/classify_cifar10.py -c ./hypertests/2.yaml
-./main.py ./runners/classify_cifar10.py -c ./hypertests/3.yaml
 ./main.py ./runners/classify_cifar10.py -c ./hypertests/preston.yaml
 ./main.py ./runners/classify_cifar10.py -c ./hypertests/1.yaml
 git add -A
