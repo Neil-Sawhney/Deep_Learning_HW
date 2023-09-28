f749b0117887f85640c95fd1c98fa1e0c14acda1
Neil Sawhney <Neil@neil-sawhney.com>
2023-09-28 11:17:51 +0000

    hypertests

--- Hw_4/hypertests/hypertests.sh
+++ Hw_4/hypertests/hypertests.sh
@@ -1,8 +1,8 @@
-./main.py ./runners/classify_cifar10.py -c ./hypertests/1.yaml
 ./main.py ./runners/classify_cifar10.py -c ./hypertests/2.yaml
 ./main.py ./runners/classify_cifar10.py -c ./hypertests/3.yaml
-./main.py ./runners/classify_cifar10.py -c ./hypertests/4.yaml
-./main.py ./runners/classify_cifar10.py -c ./hypertests/5.yaml
+./main.py ./runners/classify_cifar10.py -c ./hypertests/preston.yaml
+./main.py ./runners/classify_cifar10.py -c ./hypertests/1.yaml
 git add -A
 git commit -m "hypertests"
 git push
+sudo shutdown -h now
