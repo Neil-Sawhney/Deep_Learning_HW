39fcb9a8cfe0cbb7ab7b720e048c4eed7d63fcd8
Ubuntu <ubuntu@ip-172-31-70-41.ec2.internal>
2023-10-08 05:53:55 +0000

    hypertests: 1

--- Hw_4/hypertests/hypertests.sh
+++ Hw_4/hypertests/hypertests.sh
@@ -1,19 +1,19 @@
-./main.py ./runners/classify_cifar10.py -u ./hypertests/1.yaml
+./main.py ./runners/classify_cifar10.py -c ./hypertests/1.yaml
 git add -A
 git commit -m "hypertests: 1"
 git push
 
-./main.py ./runners/classify_cifar10.py -u ./hypertests/2.yaml
+./main.py ./runners/classify_cifar10.py -c ./hypertests/2.yaml
 git add -A
 git commit -m "hypertests: 2"
 git push
 
-./main.py ./runners/classify_cifar10.py -u ./hypertests/3.yaml
+./main.py ./runners/classify_cifar10.py -c ./hypertests/3.yaml
 git add -A
 git commit -m "hypertests: 3"
 git push
 
-./main.py ./runners/classify_cifar10.py -u ./hypertests/4.yaml
+./main.py ./runners/classify_cifar10.py -c ./hypertests/4.yaml
 git add -A
 git commit -m "hypertests: 4"
 git push
